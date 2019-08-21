62422cd1aa9985d3dc77a3d3ec110628228b54ce
kikuta-reo-FTC <r_kikuta@ft-cons.co.jp>
2019-08-21 15:32:42 +0900

    commit20190821

--- calculate.java
+++ calculate.java
@@ -1,13 +1,20 @@
-import java.math.BigInteger;
+/**
+*@author leokikuta
+*@version int -> BigInteger 
+ */
 
-public class calculate{
+
+import java.math.BigInteger;
+//バイト数の問題でBigIntegerを採用
+
+public class Calculate{
 
     public static BigInteger zero = BigInteger.valueOf(0);
     public static BigInteger second = BigInteger.valueOf(2);
-
+    //奇数判定を実施
     public static boolean isEvenValue(BigInteger figure){
-        BigInteger Answer = BigInteger.remainder(figure, second);
-        if(Answer == second){
+    BigInteger Answer = figure.remainder(second);
+        if(Answer == zero){
             return true;
         }
         return false;
--- new_first_task.java
+++ new_first_task.java
@@ -1,11 +1,10 @@
-import java .io.*;
+import java.io.*;
 import java.math.BigInteger;
 /**
 *@author Leo Kikuta
 *@version java 1.8
-*@version 繰り返し処理を追加
  */
-class new_first_task
+class New_First_Task
 {
     public static void main(String[] args) throws IOException
     {
@@ -24,12 +23,10 @@ class new_first_task
                 String str1 = kl.readLine();
                 String str2 = kl.readLine();
                 //String型なのでlong型に変換
-                /**
+                /**                 
                 *@param string型の引数から変換
                 *また、バイト数の問題上long型を採用
                  */
-                //int num1 = Integer.parseInt(str1);
-                //int num2 = Integer.parseInt(str2);
 
                 long num1 = Integer.parseInt(str1);
                 long num2 = Integer.parseInt(str2);
@@ -37,19 +34,11 @@ class new_first_task
                 BigInteger big1 = BigInteger.valueOf(num1);
                 BigInteger big2 = BigInteger.valueOf(num2);
 
-    
-                //BigInteger num3 = num2.add(num1)
-
-                //System.out.println(num3);
-                System.out.println(calculate.add(big1, big2));
+                System.out.println(Calculate.add(big1, big2));
                 
                 break;
             }
-            /**
-            *例外発生時の処理
-            *@param NumberFormatException は数値のformat関する例外
-            *e という引数にオブジェクトが格納されている。
-             */
+            //@param NumberFormatException は数値のformat関する例外
             catch(NumberFormatException e){
                 System.out.println("数字を入力してください");
             }
